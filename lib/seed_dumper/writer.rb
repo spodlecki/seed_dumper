089149fade2c18c343bcd6dacb700b2af48ad900
Kevin Edwards <kev.j.edwards@gmail.com>
2011-04-05 15:30:03 +0100

    create seeds folder if not exist

--- lib/seed_dumper/writer.rb
+++ lib/seed_dumper/writer.rb
@@ -3,6 +3,7 @@ module SeedDumper
   class Writer
     
     def self.write_data(klass_name, data_string='')
+      FileUtils.mkdir("#{Rails.root}/db/seeds/") unless File.exist?("#{Rails.root}/db/seeds/")
       seed_file_name = "#{Rails.root}/db/seeds/#{klass_name.tableize}.rb"
       
       File.open(seed_file_name, 'w') do |f|
