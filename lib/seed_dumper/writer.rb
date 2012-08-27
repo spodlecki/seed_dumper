44745ecb9b8bed5b115d32ec190e4e9283d2f680
Sergey Semyonov <slike9@rambler.ru>
2012-08-27 18:27:37 +0400

    Update lib/seed_dumper/writer.rb
    (FIX) Dump models with namespaced names

--- lib/seed_dumper/writer.rb
+++ lib/seed_dumper/writer.rb
@@ -5,6 +5,8 @@ module SeedDumper
     def self.write_data(klass_name, records)
       FileUtils.mkdir("#{Rails.root}/db/seeds/") unless File.exist?("#{Rails.root}/db/seeds/")
       seed_file_name = "#{Rails.root}/db/seeds/#{klass_name.tableize}.rb"
+      seed_file_dir = File.dirname(seed_file_name)
+      FileUtils.mkdir_p(seed_file_dir) unless File.exists?(seed_file_dir)
       
       puts "Writing #{seed_file_name}."
       File.open(seed_file_name, 'w') do |f|
