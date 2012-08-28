fd10e6c80770662e6b13ffa452f52c35304507c0
Sergey Semyonov <slike9@rambler.ru>
2012-08-28 10:39:25 +0400

    ignore option added

--- lib/seed_dumper/fetcher.rb
+++ lib/seed_dumper/fetcher.rb
@@ -5,6 +5,7 @@ module SeedDumper
     
     def self.fetch_data(klass, options={})
       ignore = ['created_at', 'updated_at']
+      ignore += options[:ignore] if options[:ignore].is_a? Array
       model_name = klass.name
       
       puts "Adding #{model_name.camelize} seeds."
